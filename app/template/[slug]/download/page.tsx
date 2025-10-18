e8098a663edc026c2e2c806513454a57e8e70852
zulfahmidev <developer.zulfahmi@gmail.com>
2025-10-18 12:30:19 +0700

    feat: update iklan

--- app/template/[slug]/download/page.tsx
+++ app/template/[slug]/download/page.tsx
@@ -1,5 +1,6 @@
 import { ReadData } from "@/lib/helper"
 import Notice from "./notice"
+import AdComponent from "@/components/ad-component"
 
 export default async function TemplateDownloadPage({params: { slug }} : {
     params: {
@@ -9,8 +10,11 @@ export default async function TemplateDownloadPage({params: { slug }} : {
     const templates = await ReadData('templates.json') as Template[]
     const data = await templates.find((v) => v.slug === slug ) as Template
     return (
-        <section className="min-h-[80vh] flex items-center justify-center flex-col px-5 lg:px-0 text-center">
+        <section className="min-h-[80vh] flex items-center justify-center flex-col px-5 lg:px-0 text-center py-32">
             <Notice data={data} />
+            <div style={{ width: '100%', minHeight: '100px' }} className="my-6">
+                <AdComponent adSlot="6605955342" adFormat="auto" adResponsive={true} />
+            </div>
         </section>
     )
 }
---
+++ components/ad-component.tsx
@@ -0,0 +1,36 @@
+"use client"
+import React, { useEffect } from 'react';
+
+interface AdComponentProps {
+  adSlot: string;
+  adFormat?: string;
+  adLayout?: string;
+  adResponsive?: boolean
+}
+
+const AdComponent: React.FC<AdComponentProps> = ({ adSlot, adFormat = 'auto', adLayout = '', adResponsive = false }) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      try {
+        (window as any).adsbygoogle = (window as any).adsbygoogle || [];
+        (window as any).adsbygoogle.push({});
+      } catch (e) {
+        console.error('Error loading ads:', e);
+      }
+    }, 300);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <ins className="adsbygoogle"
+      style={{ display: 'block', width: '100%' }}
+      data-ad-client="ca-pub-9634380513791071"
+      data-ad-slot={adSlot}
+      data-ad-format={adFormat}
+      data-full-width-responsive={adResponsive ? "true" : "false"}
+      data-ad-layout={adLayout}></ins>
+  );
+};
+
+export default AdComponent;
